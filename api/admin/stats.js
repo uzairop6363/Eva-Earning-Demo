ab75ed20f7dac9bda1e877547d7dd34ebf7faa5c
uzairop6363 <uzairop6363@gmail.com>
2026-07-29 18:58:44 +0500

    Update stats.js

--- api/admin/stats.js
+++ api/admin/stats.js
@@ -1,5 +1,22 @@
-export default function handler(req, res) {
+module.exports = function handler(req, res) {
+
+  if (req.method !== "GET") {
+    return res.status(405).json({
+      success: false,
+      message: "Method not allowed"
+    });
+  }
+
   res.status(200).json({
-    message: "API Working"
+    success: true,
+    message: "Eva Earning Admin Stats API Working",
+    stats: {
+      totalUsers: 0,
+      pendingWithdrawals: 0,
+      approvedWithdrawals: 0,
+      rejectedWithdrawals: 0,
+      totalApprovedAmount: 0
+    }
   });
-}
+
+};
